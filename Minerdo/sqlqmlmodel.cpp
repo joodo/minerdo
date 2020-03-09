93e6570000fd386aaa151fbf58f3f69b63bcd60e
lianghanzhong <wyattliang@gmail.com>
2020-03-09 18:57:21 +0800

    :bug: fix strange behavior on page EntryList after entry deleted

--- Minerdo/sqlqmlmodel.cpp
+++ Minerdo/sqlqmlmodel.cpp
@@ -28,8 +28,8 @@ void SqlQmlModel::remove(int index)
 {
     beginRemoveRows(QModelIndex(), index, index);
     removeRow(index);
-    select();
     endRemoveRows();
+    select();
 }
 
 QHash<int, QByteArray> SqlQmlModel::roleNames() const
